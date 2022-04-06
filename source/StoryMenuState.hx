b49de62cc8203c9a0ef04d4200e5265e2ce181b5
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-06 20:40:39 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -221,7 +221,11 @@ class StoryMenuState extends MusicBeatState
 		updateText();
 
 		trace("Line 165");
-
+					
+                #if mobileC
+		addVirtualPad(FULL, A_B);
+		#end
+					
 		super.create();
 	}
 
